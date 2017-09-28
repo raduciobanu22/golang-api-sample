2158f669951adc05db6500840006cddf47b25a61
Radu Ciobanu <radu.ciobanu22@gmail.com>
2017-09-28 14:48:08 +0800

    More testing

--- main_test.go
+++ main_test.go
@@ -5,6 +5,9 @@ import (
 	"testing"
 	"net/http"
 	"net/http/httptest"
+	"github.com/stretchr/testify/assert"
+	"log"
+	"encoding/json"
 )
 
 var service FXService
@@ -19,18 +22,30 @@ func TestGetAllCurrentRates(t *testing.T) {
 	request, _ := http.NewRequest("GET", "/current_rates", nil)
 	response := httptest.NewRecorder()
 	service.Router.ServeHTTP(response, request)
-	
-	if response.Code != http.StatusOK {
-		t.Errorf("Expected status code 200. Got %d\n", response.Code)
-	}
+	assert.Equal(t, response.Code, http.StatusOK, "Expected status code is 200")
+
+	var data map[string]float32
+	decoder := json.NewDecoder(response.Body)
+	decoder.Decode(&data)
+
+	assert.True(t, len(data) > 1, "Expected more than one currency rate")
 }
 
 func TestGetSingleRate(t *testing.T) {
-	request, _ := http.NewRequest("GET", "/current_rates?currency=SGD", nil)
+	currency := "SGD"
+	request, _ := http.NewRequest("GET", "/current_rates?currency=" + currency, nil)
 	response := httptest.NewRecorder()
 	service.Router.ServeHTTP(response, request)
-	
-	if response.Code != http.StatusOK {
-		t.Errorf("Expected status code 200. Got %d\n", response.Code)
-	}
+	assert.Equal(t, response.Code, http.StatusOK, "Expected status code is 200")
+
+	var data map[string]float32
+	decoder := json.NewDecoder(response.Body)
+	decoder.Decode(&data)
+	assert.Equal(t, len(data), 1, "Response should contain one currency rate")
+	assert.NotEqual(t, data[currency], 0,  "Currency rate shouldn't be 0")
+	log.Println(data[currency])
+}
+
+func TestGetNewRates(t *testing.T) {
+	//TODO Mock/Hijack the http request to openexchangerates
 }
